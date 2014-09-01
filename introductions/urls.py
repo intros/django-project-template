040c2fa7ef711e8253cd71565ce3df7d3effbd8a
Alex Lovell-Troy <alex@lovelltroy.org>
2014-09-01 09:49:25 -0600

    Adding the default boilerplate for Introduction Views

---
+++ introductions/urls.py
@@ -0,0 +1,49 @@
+# urls.py
+from django.conf.urls import url
+from introductions.views import (
+    IntroductionCreateView,
+    IntroductionDetailView,
+    IntroductionListView,
+    IntroductionUpdateView,
+
+    IntroductionEmailCreateView,
+    IntroductionEmailDetailView,
+    IntroductionEmailListView,
+    IntroductionEmailUpdateView,
+
+    IntroductionReminderCreateView,
+    IntroductionReminderDetailView,
+    IntroductionReminderListView,
+
+    PersonCreateView,
+    PersonDetailView,
+    PersonListView,
+
+    PersonUpdateView,
+    PersonEmailCreateView,
+    PersonEmailDetailView,
+    PersonEmailListView,
+    PersonEmailUpdateView,
+)
+
+urlpatterns = [
+    url(r'^introduction/$', IntroductionCreateView.as_view(), name="introduction_create"),
+    url(r'^introduction/(?P<pk>[0-9]+)/$', IntroductionDetailView.as_view(), name="introduction_detail"),
+    url(r'^introductions/$', IntroductionListView.as_view(), name="introduction_list"),
+    url(r'^introduction/(?P<pk>[0-9]+)/update/$', IntroductionUpdateView.as_view(), name="introduction_update"),
+    url(r'^introduction_email/$', IntroductionEmailCreateView.as_view(), name="introduction_email_create"),
+    url(r'^introduction_email/(?P<pk>[0-9]+)/$', IntroductionEmailDetailView.as_view(), name="introduction_email_detail"),
+    url(r'^introduction_emails/$', IntroductionEmailListView.as_view(), name="introduction_email_list"),
+    url(r'^introduction_email/(?P<pk>[0-9]+)/update/$', IntroductionEmailUpdateView.as_view(), name="introduction_email_update"),
+    url(r'^introduction_reminder/$', IntroductionReminderCreateView.as_view(), name="introduction_reminder_create"),
+    url(r'^introduction_reminder/(?P<pk>[0-9]+)/$', IntroductionReminderDetailView.as_view(), name="introduction_reminder_detail"),
+    url(r'^introduction_reminders/$', IntroductionReminderListView.as_view(), name="introduction_reminder_list"),
+    url(r'^person/$', PersonCreateView.as_view(), name="person_create"),
+    url(r'^person/(?P<pk>[0-9]+)/$', PersonDetailView.as_view(), name="person_detail"),
+    url(r'^persons/$', PersonListView.as_view(), name="person_list"),
+    url(r'^person/(?P<pk>[0-9]+)/update/$', PersonUpdateView.as_view(), name="person_update"),
+    url(r'^person_email/$', PersonEmailCreateView.as_view(), name="person_email_create"),
+    url(r'^person_email/(?P<pk>[0-9]+)/$', PersonEmailDetailView.as_view(), name="person_email_detail"),
+    url(r'^person_emails/$', PersonEmailListView.as_view(), name="person_email_list"),
+    url(r'^person_email/(?P<pk>[0-9]+)/update/$', PersonEmailUpdateView.as_view(), name="person_email_update"),
+]
--- introductions/views.py
+++ introductions/views.py
@@ -1,3 +1,71 @@
-from django.shortcuts import render
+from django.views.generic import (CreateView,
+    DetailView,
+    ListView,
+    RedirectView,
+    TemplateView,
+    UpdateView,
+)
 
-# Create your views here.
+from introductions.models import ( Introduction,
+    IntroductionEmail,
+    IntroductionReminder,
+    Person,
+    PersonEmail
+)
+
+class PersonListView(ListView):
+    model = Person
+
+class PersonDetailView(DetailView):
+    model = Person
+
+class PersonCreateView(CreateView):
+    model = Person
+
+class PersonUpdateView(UpdateView):
+    model = Person
+
+class PersonEmailListView(ListView):
+    model = PersonEmail
+
+class PersonEmailDetailView(DetailView):
+    model = PersonEmail
+
+class PersonEmailCreateView(CreateView):
+    model = PersonEmail
+
+class PersonEmailUpdateView(UpdateView):
+    model = PersonEmail
+
+class IntroductionListView(ListView):
+    model = Introduction
+
+class IntroductionDetailView(DetailView):
+    model = Introduction
+
+class IntroductionCreateView(CreateView):
+    model = Introduction
+
+class IntroductionUpdateView(UpdateView):
+    model = Introduction
+
+class IntroductionEmailListView(ListView):
+    model = IntroductionEmail
+
+class IntroductionEmailDetailView(DetailView):
+    model = IntroductionEmail
+
+class IntroductionEmailCreateView(CreateView):
+    model = IntroductionEmail
+
+class IntroductionEmailUpdateView(UpdateView):
+    model = IntroductionEmail
+
+class IntroductionReminderListView(ListView):
+    model = IntroductionReminder
+
+class IntroductionReminderDetailView(DetailView):
+    model = IntroductionReminder
+
+class IntroductionReminderCreateView(CreateView):
+    model = IntroductionReminder
