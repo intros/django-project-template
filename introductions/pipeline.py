99c429c332cc3476f1adf850b53641d1fa4b3f03
bayuadji <bayuadji@gmail.com>
2014-09-27 20:53:23 +0700

    add validation in create a new mailbox

--- introductions/pipeline.py
+++ introductions/pipeline.py
@@ -6,12 +6,19 @@ def save_mailbox(backend, user, response, *args,**kwargs):
     based on google oauth imap.
     """
     emails_response = response.get('emails', [])
+
+    #raise an exception if email not found in response from python.auth.
     if len(emails_response) > 0:
         email = emails_response[0]['value']
     else:
         raise Exception("Email not found")
-
+    
     imap_addrs = "gmail+ssl://%s:pass@imap.gmail.com" % email
-    mailbox = Mailbox.objects.create(name=email, uri=imap_addrs)
+    
+    current_mailbox_count = Mailbox.objects.filter(uri=imap_addrs).count()
+
+    #do not create a mailbox if uril already exists.
+    if current_mailbox_count == 0:
+        mailbox = Mailbox.objects.create(name=email, uri=imap_addrs)
     
     
